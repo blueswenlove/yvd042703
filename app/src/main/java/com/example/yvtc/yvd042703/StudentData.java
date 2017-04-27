8aa3d900f26e2e3814f0fe5369f1aef43888657c
blueswen <blueswenlove@gamil.com>
2017-04-27 16:41:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd042703/StudentData.java
@@ -0,0 +1,12 @@
+package com.example.yvtc.yvd042703;
+
+/**
+ * Created by YVTC on 2017/4/27.
+ */
+
+public class StudentData {
+    public String getData()
+    {
+        return "Jack";
+    }
+}
